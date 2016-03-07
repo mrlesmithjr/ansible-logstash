e1b48ff38624a0db86af1af7adbaddef82948376
Larry Smith Jr <mrlesmithjr@gmail.coml>
2016-03-07 10:47:55 -0500

    Updated Vagrant build
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.coml>

--- provision.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-apt-get update
-apt-get install -y python-dev python-pip
-pip install ansible
-mkdir -p /etc/ansible/roles
-ln -s /vagrant /etc/ansible/roles/ansible-logstash
-ansible-playbook -i "localhost," -c local /vagrant/playbook.yml
